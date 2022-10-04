430a698ca1041adac450c6a3be8d3a2c06fbb92e
Ilia Reingold <ilia.reingold@gmail.com>
2022-10-03 21:33:43 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container text-2xl">
+    test this
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="postcss">
+.container {
+  @apply bg-red-400;
+}
+</style>
